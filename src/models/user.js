55bc780422b1493523431cd995894ec0eef43644
Harold <haroldbarron13@gmail.com>
2022-08-17 15:33:18 -0500

    Making email a unique property

--- src/models/user.js
+++ src/models/user.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -42,6 +43,25 @@ const userSchema = new mongoose.Schema({
     } 
 })
 
+userSchema.statics.findByCredentials = async (email,password) =>{
+    const user = await User.findOne({email})
+
+    if (!user){
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bycrypt.compare(password,user.password)
+
+    if (!isMatch){
+        throw new Error('unable to login')
+    }
+
+    return user
+}
+
+
+
+//Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this 
 
