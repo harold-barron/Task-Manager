a794875fc9db459a95babe9a531d573dd01f7528
Harold <haroldbarron13@gmail.com>
2022-08-22 18:16:29 -0500

    Hidding private data

--- src/index.js
+++ src/index.js
@@ -6,20 +6,6 @@ const Task = require("./models/task")
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req,res,next)=>{
-//     if(req.method === 'GET'){
-//         res.send('Get request are disabled')
-//     }
-//     else{
-//         next()
-//     }
-// })
-
-// app.use((req,res,next) =>{
-    
-//     res.status(503).send("Server in maintenance")
-    
-// })
 
 const userRouter = require('../src/router/userRouter')
 const taskRouter = require('../src/router/taskRouter')
