df4b1fbaab0c4142a47c9697a3a30c3681e7f9ab
Harold <haroldbarron13@gmail.com>
2022-08-23 18:05:38 -0500

    Updating user toJSON method to remove the user avatar for the response

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require("./models/user")
-const Task = require("./models/task")
-
-const app = express()
-const port = process.env.PORT || 3000
-
-
 const userRouter = require('../src/router/userRouter')
 const taskRouter = require('../src/router/taskRouter')
 
+const app = express()
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-
-app.listen(port, ( )=>
-{
+app.listen(port, ( )=>{
     console.log('Server is up on: ', port)
-})
-
-
-const multer  = require('multer')
-
-const upload = multer ({
-    dest: 'images'
 })
