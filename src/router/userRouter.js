8bead7d7f6db4feb527c7c13f6c6cbf4567b8a0c
Harold <haroldbarron13@gmail.com>
2022-08-23 17:08:01 -0500

    Adding avatar in users schema

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -107,7 +107,9 @@ const upload = multer ({
 })
 
 router.post('/users/me/avatar',upload.single('avatar'),(req,res)=>{
-    res.status(200).send('File uploaded')
+    res.send('File uploaded')
+}, (error,req,res,next)=>{
+    res.status(400).send({error:error.message})
 })
 
 module.exports = router
