2a0452bf938bf76b403ca42ae8fb2523cebe359b
Harold <haroldbarron13@gmail.com>
2022-08-22 20:23:28 -0500

    Refactoring task model creating an explicit task schema and adding timestamps

--- src/models/task.js
+++ src/models/task.js
@@ -1,7 +1,7 @@
 const { MongoTopologyClosedError } = require('mongodb')
 const mongoose = require('mongoose')
 
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -16,6 +16,11 @@ const Task = mongoose.model('Task', {
         required: true,
         ref: 'User'
     }
-})
+},{
+    timestamps:true
+}
+)
+
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
