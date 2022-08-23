2d8a690c262729a045a0dc922c9edd3e9c540e4b
Harold <haroldbarron13@gmail.com>
2022-08-22 19:19:21 -0500

    Creating relationship between user and task

--- src/models/task.js
+++ src/models/task.js
@@ -1,3 +1,4 @@
+const { MongoTopologyClosedError } = require('mongodb')
 const mongoose = require('mongoose')
 
 const Task = mongoose.model('Task', {
@@ -9,6 +10,11 @@ const Task = mongoose.model('Task', {
     completed_field:{
         type: Boolean,
         default: false
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 })
 
