a2d4db852c355713df8d1d5d3f02b1c49697eef0
Harold <haroldbarron13@gmail.com>
2022-08-30 18:54:15 -0500

    Adding authorization to the test and testing get correctly the user profile

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOneId = new mongoose.Types.ObjectId()
+
 const userOne = {
+    _id: userOneId,
     name: 'mike',
     email: 'mike@gmail.com',
-    password: '56what!!'
+    password: '56what!!',
+    tokens: [{
+        token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async ()=>{
@@ -33,4 +41,12 @@ test('Should not login existing user', async () =>{
         email: userOne.email,
         password: "userOne.password"
     }).expect(400)
+})
+
+test('Should get profile for user', async () =>{
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
 })
