f94e764b08ac83504b14a0f5bfe0a1a267a1e106
Harold <haroldbarron13@gmail.com>
2022-08-30 18:35:41 -0500

    Creating user test file and creating first test for the creation of a new user

---
+++ tests/user.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest')
+const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'mike',
+    email: 'mike@gmail.com',
+    password: '56what!!'
+}
+
+beforeEach(async ()=>{
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+test('Should singup a new user', async () =>{
+    await request(app).post('/users').send({
+        name:'Harold',
+        email: 'harod@gmail.com',
+        password: "MyPass777!"
+    }).expect(201)
+})
