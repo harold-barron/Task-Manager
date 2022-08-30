b5e9de817c40f99b66e0ef0ec74bd752730fcc7b
Harold <haroldbarron13@gmail.com>
2022-08-30 10:45:58 -0500

    Cors config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const taskRouter = require('../src/router/taskRouter')
 const app = express()
 const port = process.env.PORT
 
+const cors = require('cors')
+const corOptions = {
+    origin: "http://localhost:8081"
+}
+app.use(cors(corOptions))
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
