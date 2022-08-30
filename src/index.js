5ce64d5c25fb58acbcf9ad3ad42cccaa028430ba
Harold <haroldbarron13@gmail.com>
2022-08-30 18:34:26 -0500

    uodatin inex.js and userRouter to make tests

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('../src/router/userRouter')
-const taskRouter = require('../src/router/taskRouter')
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-const cors = require('cors')
-const corOptions = {
-    origin: "http://localhost:8081"
-}
-app.use(cors(corOptions))
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, ( )=>{
     console.log('Server is up on: ', port)
