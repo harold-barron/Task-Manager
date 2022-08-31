73b45fbf8371a4ea9616b0b1f443a8ec8b1cf4fc
Harold <haroldbarron13@gmail.com>
2022-08-31 13:12:13 -0500

    Adding advanced asserts

--- tests/user.test.js
+++ tests/user.test.js
@@ -22,18 +22,26 @@ beforeEach(async ()=>{
 })
 
 test('Should singup a new user', async () =>{
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name:'Harold',
         email: 'harod@gmail.com',
         password: "MyPass777!"
     }).expect(201)
+
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+
 })
 
 test('Should login existing user', async () =>{
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email: userOne.email,
         password: userOne.password
     }).expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('Should not login existing user', async () =>{
@@ -63,11 +71,13 @@ test('Should delete account for user', async ()=>{
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
 })
 
 test('Should not delete account for user', async ()=>{
     await request(app)
         .delete('/users/me')
         .send()
-        .expect(401)
+        .expect(401)   
 })
